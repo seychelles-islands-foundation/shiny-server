3cbe7f878e6d731a8e16f5844c037faf98a33b61
Fernando Cagua <efcaguab@gmail.com>
2016-09-23 18:16:45 +0400

    use databases instead of rmd files

--- aldabratides/code/02_tidal-models.R
+++ aldabratides/code/02_tidal-models.R
@@ -9,13 +9,23 @@ tide_models <- lapply(tide, function(x){
   tm <- list(hc60, hc37, hc7, hc4) %>%
     lapply(function(y) ftide(x$depth, x$dateUTC, hcn = y)) 
   # chose the best by AIC
-  best <- lapply(tm, AIC) %>% unlist() %>% which.min()
+  best <-
+  	lapply(tm, AIC) %>% unlist()  %>% which.min()
   tm <- tm[[best]]
   # remove parts of the object to reduce the file size
   tm$qr <- NULL
   tm$model <- NULL
   return(tm)
   })
+# 
+# d <- data.frame(time = seq(trunc(Sys.time(), 'day'), trunc(Sys.time() + 3600*24, 'day'), 'min'))
+# 
+# tide_models[[1]] %>% plyr::ldply(function(x){
+# 	d %>% plyr::mutate(height = predict(x, from = min(d$time), to = max(d$time), 1/60), 
+# 										 h = length(x$coefficients)/2)
+# 	
+# }) %T>% ggplot2::qplot(x = time, y = height, data = ., colour = as.factor(h), geom = "line") %>%
+# 	plyr::ddply('h', get_high_low_table) %>% plyr::arrange(time)
 
 save(tide_models, file ="../data/processed/tide_models.rds", compress = T)
 
--- aldabratides/code/03_pregenerate_tidal_predictions.R
+++ aldabratides/code/03_pregenerate_tidal_predictions.R
@@ -3,10 +3,17 @@ library(foreach)
 library(parallel)
 library(doMC)
 library(TideHarmonics)
+library(RSQLite)
+library(dplyr)
 registerDoMC(cores = 4)
 
 source("../dashboard_functions.R")
 load(file ="../data/processed/tide_models.rds")
+
+file.remove('../data/processed/tide_predictions.sqlite3')
+tide_pred <- src_sqlite('../data/processed/tide_predictions.sqlite3', create = T)
+# tide_pred <- dbConnect("SQLite", dbname='./data/processed/tide_predictions.sqlite3')
+indexes_tide <- list(c('year', 'month', 'day'), 'loc_id', 'date')
 
 # predict water height for a time interval at a specified resolution
 predict_height <- function(dr, c_model, t_res = t_resolution){
@@ -18,27 +25,45 @@ predict_height <- function(dr, c_model, t_res = t_resolution){
                   time = as.POSIXct(as.POSIXlt(time, tz = "Indian/Mahe")))
 }
 
-t_res <- 10  # resolution for predictions
+t_res <- 1  # resolution for predictions
 
 # to make it more manegeable split it by month
-time_frame <- data.frame(ds = seq(as.POSIXct("2016-01-01 00:00:00", tz = "Indian/Mahe"),
-                    as.POSIXct("2018-01-01 00:00:00", tz = "Indian/Mahe"), 
+time_frame <- data.frame(ds = seq(as.POSIXct("1979-01-01 00:00:00", tz = "Indian/Mahe"),
+                    as.POSIXct("2020-01-01 00:00:00", tz = "Indian/Mahe"), 
                     "month")) %>% 
   dplyr::mutate(de = dplyr::lead(ds),
                 year =lubridate::year(ds),
                 month = lubridate::month(ds)) %>%
   dplyr::filter(!is.na(de))
 
-dir.create(paste0("../data/processed/tide_pred"))
 for(x in length(tide_models)){
   plyr::d_ply(time_frame, "year", function(y){
-    dir.create(paste0("../data/processed/tide_pred/loc_", x))
-    y %>%
+  	message('location ', x, ' - ', y$year[1])
+    o <- y %>%
       plyr::ddply("month", function(z){
         h <- predict_height(c(z$ds, z$de), tide_models[[x]], t_res)
         h[1:(nrow(h)-1), ]
-      }, .parallel = T) %>%
-      saveRDS(file = paste0("../data/processed/tide_pred/loc_", x, "/", y$year[1], ".rds"))
-  }, .progress = "text")
+      }, .progress = 'text') %>% 
+    	dplyr::select(-month) %>%
+    	dplyr::mutate(year = as.integer(lubridate::year(time)),
+    								month = as.integer(lubridate::month(time)),
+    								day = as.integer(lubridate::day(time)),
+    								date = as.integer(as.numeric(time)), 
+    								time_n = as.integer(as.numeric(difftime(time, trunc(time, 'day'), units = 'sec'))),
+    								loc_id = as.integer(x)) %>% 
+    	dplyr::select(-time)
+    if (length(dbListTables(tide_pred$con))== 0) {
+    	copy_to(tide_pred, 
+    					df = o, name = 'tide_prediction', temporary = FALSE,
+    					indexes = indexes_tide)
+    	copy_to(tide_pred, 
+    					df = get_high_low_table(o), name = 'tide_high_low', temporary = FALSE,
+    					indexes = indexes_tide)
+    } else {
+    	tide_pred <- src_sqlite('../data/processed/tide_predictions.sqlite3', create = T)
+    	dbWriteTable(tide_pred$con, 'tide_prediction', o, append = TRUE, row.names = F)
+    	dbWriteTable(tide_pred$con, 'tide_high_low', get_high_low_table(o), append = TRUE, row.names = F)
+    }
+  })
 }
   
